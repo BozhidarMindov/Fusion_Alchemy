83501759158d795203b94aa3e505d96779376e4f
Bozhidar Mindov <bbm210@aubg.edu>
2023-10-01 13:16:50 +0300

    Update backend to use local stable diffusion

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,12 +1,41 @@
+import os
+from uuid import uuid4
+
 from fastapi import FastAPI
 import uvicorn
+import torch
+from diffusers import StableDiffusionPipeline
+
 
 app = FastAPI()
 
+# Load the Stable Diffusion model
+SDV5_MODEL_PATH = os.environ.get("SDV5_MODEL_PATH")
+pipe = StableDiffusionPipeline.from_pretrained(SDV5_MODEL_PATH,
+                                               torch_dtype=torch.float16)
+pipe = pipe.to("cuda")
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+
+def generate_image(pipe, name):
+    try:
+        base_prompt = f"""
+        In a future where avatars reflect personality,
+        generate an avatar for {name}, a human explorer.
+        Realistic, sense of awe, cool, futuristic, inspiring, beautiful, tech, cyber, clear face, no blur, cyberpunk
+        """
+        # Generate an image based on the provided prompt
+        image = pipe(base_prompt).images[0]
+        # Save the image
+        image.save(f"images/{uuid4()}.png")
+        return {"message": "Avatar generated successfully"}
+    except Exception as e:
+        return {"message": "An error occurred while generating the avatar", "error": str(e)}
+
+
+@app.get("/generate")
+async def root(name: str):
+    return generate_image(pipe, name)
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, reload=True, port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
