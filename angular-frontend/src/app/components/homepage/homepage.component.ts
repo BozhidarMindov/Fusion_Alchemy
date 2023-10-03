ebd7996f2e6a685c1fb19ed2415799883ff21d1c
Bozhidar Mindov <bbm210@aubg.edu>
2023-10-03 23:18:15 +0300

    Add avatar regeneration and download functionality

--- angular-frontend/src/app/components/homepage/homepage.component.ts
+++ angular-frontend/src/app/components/homepage/homepage.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -11,6 +10,9 @@ export class HomepageComponent {
   userName: string = '';
   avatarImage: string | null = null;
 
+  // Store the last used username for regeneration
+  lastUsedUserName: string | null = null;
+
   constructor(private http: HttpClient) {}
 
   generateAvatar() {
@@ -19,6 +21,8 @@ export class HomepageComponent {
       return;
     }
 
+    // Store the last used username
+    this.lastUsedUserName = this.userName;
     // Send the user's name to the backend to generate the avatar
     this.http.post<any>('http://localhost:8000/generate', { "name": this.userName }).subscribe(
       (response) => {
@@ -30,5 +34,43 @@ export class HomepageComponent {
         // Handle error (display an error message, if needed)
       }
     );
+  }
+
+  regenerateAvatar() {
+    if (this.lastUsedUserName) {
+      // Use the last used username for regeneration
+      this.userName = this.lastUsedUserName;
+      this.generateAvatar(); // Reuse the generateAvatar function
+    } else {
+      // Handle case where there's no previous username to regenerate
+        console.error('Error generating avatar');
+    }
+  }
+
+  downloadAvatar() {
+    if (this.avatarImage) {
+      // Convert the avatar image URL to a blob
+      fetch(this.avatarImage)
+        .then(response => response.blob())
+        .then(blob => {
+          // Create an object URL from the blob
+          const blobURL = URL.createObjectURL(blob);
+
+          // Create an anchor element to trigger the download
+          const link = document.createElement('a');
+          link.href = blobURL;
+          link.download = 'avatar.png'; // Set the filename for download
+          link.click();
+
+          // Release the object URL to free resources
+          URL.revokeObjectURL(blobURL);
+        })
+        .catch(error => {
+          console.error('Error generating avatar:', error);
+        });
+    } else {
+      // Handle case where there's no avatar image to download
+      console.error('Error generating avatar');
+    }
   }
 }
